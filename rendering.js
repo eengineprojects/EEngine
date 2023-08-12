9f717f98cdca98c400d705e9fbb36a94d894cbd8
Aadi K <aadi_k@outlook.com>
2023-08-12 12:11:28 -0500

    editted the code

--- rendering.js
+++ rendering.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById('render');
     const scene = new THREE.Scene();
     const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('render') });
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const raycaster = new THREE.Raycaster();
     const pointer = new THREE.Vector2();
+    const canvasBounds = canvas.getBoundingClientRect();
+
+
     
     renderer.setSize(renderer.domElement.clientWidth, renderer.domElement.clientHeight);
   
@@ -11,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
+    var selectedobject;
     
     camera.position.z = 5;
     cube.name = "cube";
@@ -27,21 +32,23 @@ document.addEventListener('DOMContentLoaded', function() {
     //     console.log(event);
     // })
     function onMouseMove(event){
-        pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-        pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+        pointer.x = ((event.clientX - canvasBounds.left) / canvas.clientWidth) * 2 - 1;
+        pointer.y = -((event.clientY - canvasBounds.top) / canvas.clientHeight) * 2 + 1;
+        
 
         raycaster.setFromCamera(pointer, camera);
         const intersect = raycaster.intersectObjects(scene.children);
         
         if(intersect.length > 0){
+            selectedobject = intersect[0].object;
             intersect[0].object.material.color.set(0x00ffcc);
             
         }else {
-            cube.material.color.set(0x00ff00);
+            selectedobject.material.color.set(0x00ff00);
         }
 
     }
-    this.addEventListener('click', onMouseMove);
+    canvas.addEventListener('click', onMouseMove);
     // renderer.domElement.addEventListener('click', onclick, true);
     // var selectedobject;
     // var raycaster = new THREE.Raycaster();
